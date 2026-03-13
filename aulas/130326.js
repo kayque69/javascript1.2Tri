bbd51bf7131af277f71740a4396eeb3b6e6d1d64
kayque69 <kakavieira394@gmail.com>
2026-03-13 07:52:05 -0300

    create.aulas//130326

---
+++ aulas/130326.js
@@ -0,0 +1,11 @@
+let lucro = 30;
+let lucro2 = 10;
+
+function soma(a,b){
+    return a + b
+}
+soma(variavel,nome);
+
+function resutadoDivisaoSocio(lucro,lucro2){
+        return soma(lucro,lucro2)/2
+}
