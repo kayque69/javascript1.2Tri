8bb74f7d37c65657020c4bd84419767ec0fbf98b
kayque69 <kakavieira394@gmail.com>
2026-03-20 07:53:36 -0300

    update

--- aulas/190326.js
+++ aulas/190326.js
@@ -7,3 +7,24 @@ let numero = 30
 let decimal =20.30
 //string
 let nome = "não sei, você que sabe ?"
+let nome1 = "NÃO SEI, VOCÊ QUE SABE?"
+
+
+console.log(nome === nome1); // false
+
+const inputMinusculo = nome1.toLowerCase();
+
+console.log(nome === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 12 + 1 caracteres
+
+
+
+
+let nv = true;
+let nz = false;
+
+if(nv!==nz){
+    console.log(`nv é diferente, pois ele é ${nv}`)
+}
