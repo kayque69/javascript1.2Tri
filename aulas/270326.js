97b3a6f51d86bcd28a05596dcf1e2c3be257c305
kayque69 <kakavieira394@gmail.com>
2026-03-27 07:56:29 -0300

    update

---
+++ aulas/270326.js
@@ -0,0 +1,13 @@
+//Criar Array 
+
+const fila = [1,2,3,4,5,6]
+
+const filaStringAmigos = ["nicolau","jennifer","maria","rays","renan"]
+
+function lerAmigos(filaStringAmigos){
+    console.log(´você tem $(filaStringAmigos.length} amigos`)
+
+        for(let i = 0; i < filaStringAmigos.length;i++){
+        console.log(filaStringAmigos[i])
+        }
+}
